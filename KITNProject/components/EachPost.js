220ca1f2a2cea9d9071134ccf36ecabcd6da6d5c
Karen <yong-zhen.lai@epitech.eu>
2022-05-19 01:11:53 +0200

    subReddit page

--- KITNProject/components/EachPost.js
+++ KITNProject/components/EachPost.js
@@ -4,19 +4,25 @@ import { Button, Text } from 'react-native';
 function EachPost(props) {
   return (
     <>
-        <Text>I am Each post !!</Text>
+        {/* <Text>I am Each post !!</Text> */}
 
         {/* {props.post.length>0 &&
       console.log('here', props.post)} */}
-        <Text>{props.data.subreddit}</Text>
-        <Button
-          title="Go to SubredditPage"
-          onPress={() => props.navigation.navigate('SubredditScreen')}
+        {/* <Text>{props.data.subreddit}</Text> */}
+        
+        <Button 
+          title={props.data.subreddit_name_prefixed}
+          onPress={() => {
+          props.navigation.navigate('SubredditScreen', {
+            subRedditName: props.data.subreddit_name_prefixed,
+          });
+        }}
         />
         <Button
-          title="Go to PostPage"
+          title={props.data.title}
           onPress={() => props.navigation.navigate('PostScreen')}
         />
+        <Text>=======================================</Text>
     </>
   );
 }
