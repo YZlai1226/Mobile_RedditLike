0de5df40ecf77ec472cd16526e0edca97eefb4fc
Nicolas Bretécher <bretechern@gmail.com>
2022-05-22 22:35:31 +0200

    fix sub view style + add sub filters

--- KITNProject/screens/SubredditScreen.js
+++ KITNProject/screens/SubredditScreen.js
@@ -4,6 +4,8 @@ import { Button, View, ScrollView, Text, StyleSheet, Image } from 'react-native'
 import axios from 'axios';
 import PostsManager from '../components/PostsManager';
 import datetime from 'react-datetime';
+import { ButtonGroup, Layout } from '@ui-kitten/components';
+
 
 function SubredditScreen({ route, navigation }) {
   const { subRedditName } = route.params;
@@ -14,7 +16,7 @@ function SubredditScreen({ route, navigation }) {
   const [unixDate, setUnixDate] = useState();
   const [finalDate, setFinalDate] = useState();
   const [filter, setFilter] = useState('');
-  
+
   async function getFilteredPosts() {
     const res = await axios.get(`https://www.reddit.com/${subRedditName}/${filter}.json`);
     setSubPosts(res.data.data.children);
@@ -46,19 +48,49 @@ function SubredditScreen({ route, navigation }) {
 
   return (
     <ScrollView>
+      <Layout style={styles.container}>
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <View style={styles.banner}>
           <Text style={styles.title}>{subTitle}</Text>
           <Text style={styles.date}>created on {finalDate}</Text>
-          <Text>{subDes}</Text>
+          <Text style={{ color: 'white' }}>{subDes}</Text>
         </View>
+
+        {/* Filters */}
         <View>
-          
+          <ButtonGroup>
+            <Button
+              onPress={() => setFilter('new')}
+              title="New"
+            />
+
+            <Button
+              onPress={() => setFilter('best')}
+              title="Best"
+            />
+
+            <Button
+              onPress={() => setFilter('top')}
+              title="Top"
+            />
+
+            <Button
+              onPress={() => setFilter('controversial')}
+              title="Controversial"
+            />
+
+            <Button
+              onPress={() => setFilter('rising')}
+              title="Rising"
+            />
+          </ButtonGroup>
         </View>
+
         {subPosts.length > 0 &&
           <PostsManager navigation={navigation} posts={subPosts} />
         }
       </View>
+      </Layout>
     </ScrollView>
   );
 }
@@ -73,11 +105,13 @@ const styles = StyleSheet.create({
   },
   title: {
     fontWeight: 'bold',
-    fontSize: '18px'
+    fontSize: 18,
+    color: 'white'
   },
   date: {
     color: 'gray',
     fontStyle: 'italic',
-    fontSize: '10px'
+    fontSize: 10,
+    color: 'white'
   }
 });
