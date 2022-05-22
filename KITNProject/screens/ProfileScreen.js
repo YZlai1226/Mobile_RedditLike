7779b88d2d3395c0d75151106c27253a30f49f59
Nicolas Bretécher <bretechern@gmail.com>
2022-05-22 18:56:15 +0200

    fix to profile screen

--- KITNProject/screens/ProfileScreen.js
+++ KITNProject/screens/ProfileScreen.js
@@ -2,110 +2,117 @@ import * as React from 'react';
 import { Button, Text, View, SafeAreaView, StyleSheet, TouchableOpacity, Image, TextInput, ScrollView } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { FlatList } from 'react-native';
 import CommentsManager from '../components/CommentsManager';
 
-
 function UserProfile() {
+  const [token, setToken] = React.useState('');
   const [UserData, setUserData] = React.useState([]);
-
-  async function GetUserData() {
-    console.log('Fetching user data', data);
-    const data = await axios.get("https://oauth.reddit.com/api/v1/me", {
-      headers: { 'Authorization': 'Bearer 9458804-gxDmVtNslA9TDbVYbv0mRm1A1I7xdw ' }
-    })
-    setUserData(data.data)
-  }
-
-  React.useEffect(() => {
-    GetUserData()
-  },
-    []);
-
   const [UserActivity, setUserActivity] = React.useState([]);
 
+  async function retrieveToken() {
+    const retrievedToken = await AsyncStorage.getItem('@access_token');
+    setToken(retrievedToken);
+  }
+  async function GetUserData() {
+    try {
+      const res = await axios.get("https://oauth.reddit.com/api/v1/me.json", {
+        headers: { 'Authorization': 'Bearer ' + token }
+      })
+      console.log('res.data:', res.data);
+      setUserData(res.data)
+    } catch {
+      console.log('data fetching failed');
+    }
+  }
   async function GetUserActivity() {
-    let pseudo = UserData.name
-    const UserA = await axios.get("https:/reddit.com/user/" + pseudo + ".json", {
-      headers: { 'Authorization': 'Bearer 9458804-gxDmVtNslA9TDbVYbv0mRm1A1I7xdw ' }
-    })
-    setUserActivity(UserA.data.data.children)
-    // console.log("++++++++++++++++++++++++++++++++++++++++++", UserActivity.data.data.children);
-    console.log("////////////////////////", UserActivity);
+    try {
+      const res = await axios.get("https:/reddit.com/user/" + UserData.name + ".json", {
+        headers: { 'Authorization': 'Bearer ' + token }
+      });
+      setUserActivity(res.data.data.children)
+      // console.log("++++++++++++++++++++++++++++++++++++++++++", UserActivity.data.data.children);
+      console.log("////////////////////////", UserActivity);
+    } catch {
+      console.log('User activity fetching failed');
+    }
   }
 
-  React.useEffect(() => {
-    GetUserActivity()
-  },
-    [UserData]);
+  React.useEffect(() => { retrieveToken() }, []);
 
-    console.log('UserData', UserData);
-    console.log('UserActivity', UserActivity);
+  React.useEffect(() => { GetUserData() }, [token]);
 
-  return (
-    <View>
-      <View style={{
-        flexDirection: 'row',
-        marginTop: "5%",
-        marginLeft: "5%",
-        marginRight: "5%",
+  /* React.useEffect(() => { GetUserActivity() }, [UserData]); */
 
-      }}>
-
-        <TouchableOpacity onPress={() => console.log("image pressed")}>
-          <Image
-            source={{
-              width: 38,
-              height: 60,
-              uri: UserData.snoovatar_img
-            }}
-          />
-        </TouchableOpacity>
-
+  if (UserData) {
+    return (
+      <View>
         <View style={{
+          flexDirection: 'row',
+          marginTop: "5%",
           marginLeft: "5%",
-          marginTop: "5%"
+          marginRight: "5%",
+  
         }}>
-
-          <View>
-            <Text>{UserData.name}</Text>
+  
+          <TouchableOpacity onPress={() => console.log("image pressed")}>
+            <Image
+              source={{
+                width: 38,
+                height: 60,
+                uri: UserData?.snoovatar_img
+              }}
+            />
+          </TouchableOpacity>
+  
+          <View style={{
+            marginLeft: "5%",
+            marginTop: "5%"
+          }}>
+  
+            <View>
+              <Text>{UserData?.name}</Text>
+            </View>
+  
+            <View>
+              <Text>{UserData?.total_karma} Karma points</Text>
+            </View>
+  
+            {/* <View>
+              <Text>Description : {UserData.subreddit.public_description}</Text>
+            </View> */}
           </View>
-
-          <View>
-            <Text>{UserData.total_karma} Karma points</Text>
+  
+  
+          <View style={{
+            marginLeft: "59%",
+            position: 'absolute'
+          }}>
+            <Button
+              color="orange"
+              title="Edit"
+              onPress={() => console.log("button pressed")}
+            />
           </View>
-
-          {/* <View>
-            <Text>Description : {UserData.subreddit.public_description}</Text>
-          </View> */}
-
-
-
         </View>
-
-
-        <View style={{
-          marginLeft: "59%",
-          position: 'absolute'
-        }}>
-          <Button
-            color="orange"
-            title="Edit your profile"
-            onPress={() => console.log("button pressed")}
-          />
-        </View>
+  
+  
+        {/* <ScrollView >
+          {UserActivity.length > 0 &&
+          <CommentsManager comments={UserActivity} />}
+        </ScrollView> */}
+  
+  
       </View>
+    )
+  } else {
+    return (
+      <View>
+        <Text>Loading user data...</Text>
+      </View>
+    )
+  }
 
-
-      <ScrollView >
-        {UserActivity.length > 0 &&
-        <CommentsManager comments={UserActivity} />}
-      </ScrollView>
-
-
-    </View>
-  )
-}
+  }
 
 function UserPosts(props) {
 
